b7887a7b1951de719053defffc8ea7fc571474f0
Carson Farmer <carson.farmer@gmail.com>
2021-01-18 14:57:12 -0800

    fix(null): ensure val != null
    null is also an object, so need an extra check here in some cases

--- src/table.js
+++ src/table.js
@@ -454,7 +454,7 @@ class Table extends SQLBase {
         const value = row.block.cid
         let val = row.getIndex(i)
         let path
-        while (typeof val === 'object') {
+        while (typeof val === 'object' && val != null) {
           path = val.path
           const block = await get(val.link)
           row = await this.createRow({ block })
